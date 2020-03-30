61f900260f2cdd83d2540477e3074e63731446eb
AstridSuhartono <astrid_sh@yahoo.com>
2020-03-30 20:25:22 +1030

    add more code

--- test.js
+++ test.js
@@ -1,32 +1,68 @@
-function getdata(){
+$(document).ready(function() {
+var googleAPIkey = "18c881248ae94a0eb9c6e2320f2ab227";
+var bingURL = "https://cors-anywhere.herokuapp.com/https://bing.com/covid/data/";
+
+
+function displayCaseData(){
+   
     $.ajax({
-        url: "https://cors-anywhere.herokuapp.com/https://bing.com/covid/data/",
-        method: "GET",
-        success: function(response){
-            //console.log(response)
-            console.log(response.areas[18]);
-            totalCases = response.areas[18].areas;
-            totalCases.forEach((casePerSate) => console.log(`${casePerSate.displayName} ${casePerSate.totalConfirmed}`))
-        },
-        error: function(err){
-            console.log("Cannot retrieve data");
-        }  
-    })
+        url: bingURL,
+        method: "GET"})
+        .then(getBingData)
+        .fail(errormsg);
+
+    for ( var i = 0; i < response.areas.length; i++){   
+        //$("#stateName").text(JSON.stringify(response.areas[i].displayName);
+        //$("#totalConfirmed").text(JSON.stringify(response.areas[i].totalConfirmed);
+
+     }
+    
+ 
 }
-getdata();
+
+displayCaseData();
 
 
-function AUTotal() {
+function getTopNewsAU(){
     $.ajax({
-        url: "https://corona-api.com/countries/AU",
+        url: "https://newsapi.org/v2/top-headlines?country=au&apiKey="+googleAPIkey,
         method: "GET",
-        success: function(incomingData){
-
-            console.log(incomingData);
-        },
-        error: function(err) {
-            console.log("error");
-        }
-    })
+        method: "GET"})
+        .then(populateInfo)
+        .fail(errormsg);
 }
-AUTotal();
+
+getTopNewsAU();
+
+});
+
+function getBingData(response){
+    let AuIndex = response.areas.findIndex( area => area.id === "australia");
+    console.log(AuIndex);
+    data = response.areas[AuIndex];
+    console.log(data);
+    //totalCases = response.areas[AuIndex].areas;
+    //totalCases.forEach((casePerState) => console.log(`${casePerState.displayName} ${casePerState.totalConfirmed}`))
+}
+
+function populateInfo(response){
+    console.log(response)
+}
+
+function errormsg(){
+    console.log("Unable to get any data")
+}
+
+
+
+
+// idk yet optional ??
+// function AUTotal() {
+//     $.ajax({
+//         url: "https://corona-api.com/countries/AU",
+//         method: "GET"})
+//         .then(populateInfo)
+//         .fail(errormsg);
+// }
+
+// //AUTotal();
