8327e95a858a41e2d75d48f0e3a7e4bedda405be
AstridSuhartono <astrid_sh@yahoo.com>
2020-04-01 19:43:55 +1030

    add code to show in html

--- script.js
+++ script.js
@@ -1,63 +1,42 @@
 $(document).ready(function() {
     var googleAPIkey = "18c881248ae94a0eb9c6e2320f2ab227";
+    var rapidAPIkey = "3368eb3a0fmsh6fa4c6e2177a0d6p15bd2djsn0fd20a6e49da";
 
-    // getting the bing data for covid-19 cases
-    //getBingData();
+    createRapidApiData()
 
-    function getBingData(){
-        bingURL = "https://cors-anywhere.herokuapp.com/https://bing.com/covid/data";
-        $.ajax({
-            url: bingURL,
-            method: "GET"})
-            .then(populateStateInfo)
-            .fail(errormsg);
-    }
-    
-    function populateStateInfo(response){
-        let AuIndex = response.areas.findIndex( area => area.id === "australia");
-        console.log(AuIndex);
-        data = response.areas[AuIndex];
-        console.log(data);
-        let i = 0;
-        //for ( var i = 0; i < 8; i++){   
-            $("#stateName").text(JSON.stringify(data.areas[i].displayName));
-            console.log(data.areas[i].displayName);
-            $("#totalConfirmed").text(JSON.stringify(data.areas[i].totalConfirmed));
-            $("#totalDeaths").text(JSON.stringify(data.areas[i].totalDeaths));
-            $("#totalRecovered").text(JSON.stringify(data.areas[i].totalRecovered));
-            let getTime  = JSON.stringify(data.areas[i].lastUpdated);
+    //getting data from RapidApi
+    function createRapidApiData(){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Australia",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+                "x-rapidapi-key": "3368eb3a0fmsh6fa4c6e2177a0d6p15bd2djsn0fd20a6e49da"
+            }
+        }
+        $.ajax(settings).done(getCovidData).fail(errormsg);
+        };
+
+
+    // display the data inside the html
+    function getCovidData(response){
+        console.log(response);
+        let statsAustralia = response.data.covid19Stats;
+        let stateIndex = statsAustralia.findIndex( area => area.province === "South Australia");
+        console.log(stateIndex);
+//     console.log(data);   
+            let state = $("<p>").text("State: " + JSON.stringify(statsAustralia[stateIndex].province));
+            //console.log(state);
+            let confirmed = $("<p>").text("Confirmed: " + JSON.stringify(statsAustralia[stateIndex].confirmed));
+            let deaths = $("<p>").text("Deaths: " + JSON.stringify(statsAustralia[stateIndex].deaths));
+            let recovered = $("<p>").text("Recovered: " + JSON.stringify(statsAustralia[stateIndex].recovered));
+            let getTime  = $("<p>").text("Last updated: " + JSON.stringify(statsAustralia[stateIndex].lastUpdate));
             //console.log(getTime);
-            $("#lastUpdated").text(getTime);
-         //}
-        
-    }
-    
-    // createRapidApiData()
+            $("#covidDisplay").append(state,confirmed,deaths,recovered,getTime);
 
-    // //getting data from RapidApi
-    // function createRapidApiData(){
-    //     var settings = {
-    //         "async": true,
-    //         "crossDomain": true,
-    //         "url": "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Australia",
-    //         "method": "GET",
-    //         "headers": {
-    //             "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
-    //             "x-rapidapi-key": "3368eb3a0fmsh6fa4c6e2177a0d6p15bd2djsn0fd20a6e49da"
-    //         }
-    //     }
-    //     $.ajax(settings).done(getCovidData).fail(errormsg);
-    //     };
-
-    
-    // function getCovidData(response){
-    //     let statsAustralia = response.data.covid19Stats;
-
-    //     statsAustralia.forEach(state => {
-    //         // console.log(state)
-    //         console.log(`state: ${state.province} -- confirmed: ${state.confirmed} -- recovered: ${state.recovered}`);
-    //     }
-    // };
+    };
 
     // getting the top news from google news for australia
     getTopNewsAU();
@@ -89,7 +68,7 @@ $(document).ready(function() {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
-            "x-rapidapi-key": "3368eb3a0fmsh6fa4c6e2177a0d6p15bd2djsn0fd20a6e49da"
+            "x-rapidapi-key": rapidAPIkey
         }
     }
     $.ajax(settings).done(function (response) {
