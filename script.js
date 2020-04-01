7d843086862a4e84565f3348935dab651503ae1e
Karthik Kovi <Karthi.kovi@gmail.com>
2020-04-01 20:31:50 +1030

    Added script to update Australia news articles in the HTML page. Also created a separate JS file for newsArtciles

--- script.js
+++ script.js
@@ -5,15 +5,30 @@ function getAusArticles() {
         method: "GET",
         success: function (incomingData) {
             // console.log(incomingData.articles);
-            incomingData.articles.forEach((article) => {
-                    console.log(article.title)
-                    console.log(article.description)                
-            });
+
+            let newsArticles = incomingData.articles;
+
+            document.getElementById("AusnewsOne").textContent = newsArticles.title
+
+
+            for (let i = 0; i < 3; i++) {
+
+                console.log(newsArticles[i].title);
+                console.log(newsArticles[i].content);
+                console.log(newsArticles[i].url)
+
+                // newsArticles.forEach((article) => {
+                //     console.log(article.title)
+                //     console.log(article.description)
+                // })
+            }
+
         },
         error: function (uvData) {
             //console.log(uvData);
             console.log("There was an error");
         }
+
     })
 }
 
@@ -23,44 +38,78 @@ function getAusConfirmedCases() {
     $.ajax({
         url: "https://corona-api.com/countries/AU",
         method: "GET",
-        success: function(incomingData){
+        success: function (incomingData) {
             let confirmedCases = incomingData.data.latest_data.confirmed
             let recoveredCases = incomingData.data.latest_data.recovered
 
             console.log(confirmedCases);
             console.log(recoveredCases)
         },
-        error: function(uvData) {
+        error: function (uvData) {
             console.log(error);
         }
     })
 }
 
 // Function to get total confirmed and recovered cases world wide
-function getWorldConfirmedCases(){
+function getWorldConfirmedCases() {
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Australia",
+        "url": "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=*",
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
             "x-rapidapi-key": "3368eb3a0fmsh6fa4c6e2177a0d6p15bd2djsn0fd20a6e49da"
         }
     }
-    
+
     $.ajax(settings).done(function (response) {
         let statsAustralia = response.data.covid19Stats
 
-        statsAustralia.forEach(state => {
-            // console.log(state)
-            console.log(`state: ${state.province} -- confirmed: ${state.confirmed} -- recovered: ${state.recovered}`);
+        let worldWideStats = response.data.covid19Stats;
+
+        let confirmed = 0;
+        let recovered = 0;
+
+        worldWideStats.forEach(indobj => {
+            confirmed += indobj.confirmed;
+            recovered += indobj.recovered;
         })
+
+        console.log(confirmed)
+        console.log(recovered)
+
+        // console.log(response.data.covid19Stats)
+
+        // statsAustralia.forEach(state => {
+        //     // console.log(state)
+        //     console.log(`state: ${state.province} -- confirmed: ${state.confirmed} -- recovered: ${state.recovered}`);
+        // })
     });
-    
+
 }
 
 
-// getAusArticles();
-getWorldConfirmedCases();
+getAusArticles();
+// getWorldConfirmedCases();
 // getAusConfirmedCases();
+
+
+// $.ajax({
+//     url: "https://corona-api.com/countries",
+//     method: "GET",
+//     success: function (incomingData) {
+//         console.log(incomingData);
+//         var currentNo = 0;
+//         console.log()
+//         for (var i = 0; i < 249; i++) {
+//             currentNo += incomingData.data[i].latest_data.confirmed;
+//         }
+//         console.log(currentNo);
+//     },
+//     error: function (uvData) {
+//         //console.log(uvData);
+//         console.log("There was an error");
+//     }
+// })
