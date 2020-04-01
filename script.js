e5b1038354503c27ab1e9cb1eee19607e171e29d
Karthik Kovi <Karthi.kovi@gmail.com>
2020-04-01 20:56:41 +1030

    merged with the master

--- script.js
+++ script.js
@@ -5,30 +5,15 @@ function getAusArticles() {
         method: "GET",
         success: function (incomingData) {
             // console.log(incomingData.articles);
-
-            let newsArticles = incomingData.articles;
-
-            document.getElementById("AusnewsOne").textContent = newsArticles.title
-
-
-            for (let i = 0; i < 3; i++) {
-
-                console.log(newsArticles[i].title);
-                console.log(newsArticles[i].content);
-                console.log(newsArticles[i].url)
-
-                // newsArticles.forEach((article) => {
-                //     console.log(article.title)
-                //     console.log(article.description)
-                // })
-            }
-
+            incomingData.articles.forEach((article) => {
+                    console.log(article.title)
+                    console.log(article.description)                
+            });
         },
         error: function (uvData) {
             //console.log(uvData);
             console.log("There was an error");
         }
-
     })
 }
 
@@ -38,78 +23,37 @@ function getAusConfirmedCases() {
     $.ajax({
         url: "https://corona-api.com/countries/AU",
         method: "GET",
-        success: function (incomingData) {
+        success: function(incomingData){
             let confirmedCases = incomingData.data.latest_data.confirmed
             let recoveredCases = incomingData.data.latest_data.recovered
 
             console.log(confirmedCases);
             console.log(recoveredCases)
         },
-        error: function (uvData) {
+        error: function(uvData) {
             console.log(error);
         }
     })
 }
 
 // Function to get total confirmed and recovered cases world wide
-function getWorldConfirmedCases() {
-    const settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=*",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
-            "x-rapidapi-key": "3368eb3a0fmsh6fa4c6e2177a0d6p15bd2djsn0fd20a6e49da"
-        }
-    }
+function getWorldConfirmedCases(){
+    $.ajax({
+        url: "https://cors-anywhere.herokuapp.com/https://bing.com/covid/data/",
+        method: "GET",
+        success: function(response){
+            console.log(response.totalConfirmed);
+            console.log(response.totalRecovered);
 
-    $.ajax(settings).done(function (response) {
-        let statsAustralia = response.data.covid19Stats
-
-        let worldWideStats = response.data.covid19Stats;
-
-        let confirmed = 0;
-        let recovered = 0;
-
-        worldWideStats.forEach(indobj => {
-            confirmed += indobj.confirmed;
-            recovered += indobj.recovered;
-        })
-
-        console.log(confirmed)
-        console.log(recovered)
-
-        // console.log(response.data.covid19Stats)
-
-        // statsAustralia.forEach(state => {
-        //     // console.log(state)
-        //     console.log(`state: ${state.province} -- confirmed: ${state.confirmed} -- recovered: ${state.recovered}`);
-        // })
-    });
-
+            totalCases = response.areas[18].areas;
+            totalCases.forEach((casePerSate) => console.log(`${casePerSate.displayName} ${casePerSate.totalConfirmed}`))
+        },
+        error: function(err){
+            console.log("Cannot retrieve data");
+        }  
+    })
 }
 
-
-getAusArticles();
-// getWorldConfirmedCases();
+// getAusArticles();
+getWorldConfirmedCases();
 // getAusConfirmedCases();
-
-
-// $.ajax({
-//     url: "https://corona-api.com/countries",
-//     method: "GET",
-//     success: function (incomingData) {
-//         console.log(incomingData);
-//         var currentNo = 0;
-//         console.log()
-//         for (var i = 0; i < 249; i++) {
-//             currentNo += incomingData.data[i].latest_data.confirmed;
-//         }
-//         console.log(currentNo);
-//     },
-//     error: function (uvData) {
-//         //console.log(uvData);
-//         console.log("There was an error");
-//     }
-// })
