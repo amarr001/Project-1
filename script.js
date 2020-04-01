380a222ec70e2be485dc6cf4ce12c90ea41e043a
AstridSuhartono <astrid_sh@yahoo.com>
2020-04-01 21:27:06 +1030

    adding covid number

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+var googleAPIkey = "18c881248ae94a0eb9c6e2320f2ab227";
+var rapidAPIkey = "3368eb3a0fmsh6fa4c6e2177a0d6p15bd2djsn0fd20a6e49da";
+
+    createRapidApiData()
+
+    //getting data from RapidApi
+    function createRapidApiData(){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=Australia",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+                "x-rapidapi-key": rapidAPIkey
+            }
+        }
+        $.ajax(settings).done(getCovidData).fail(errormsg);
+        };
+
+
+    // display the data inside the html
+    function getCovidData(response){
+        console.log(response);
+        let statsAustralia = response.data.covid19Stats;
+        $("#AusStates").change(function(){
+            var stateText = $("#AusStates option:selected").text();
+            console.log(stateText);
+            let stateIndex = statsAustralia.findIndex( area => area.province === stateText)
+            console.log(stateIndex);
+            //console.log(data);   
+            let state = $("<p>").text("State: " + JSON.stringify(statsAustralia[stateIndex].province));
+            //console.log(state);
+            let confirmed = $("<p>").text("Confirmed: " + JSON.stringify(statsAustralia[stateIndex].confirmed));
+            let deaths = $("<p>").text("Deaths: " + JSON.stringify(statsAustralia[stateIndex].deaths));
+            let recovered = $("<p>").text("Recovered: " + JSON.stringify(statsAustralia[stateIndex].recovered));
+            let getTime  = $("<p>").text("Last updated: " + JSON.stringify(statsAustralia[stateIndex].lastUpdate));
+            //console.log(getTime);
+            $("#covidDisplay").prepend(state,confirmed,deaths,recovered,getTime);
+        })
+
+    };
+
 // Function to get the articles related to Corona Virus
 function getAusArticles() {
     $.ajax({
