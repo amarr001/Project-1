2c48ba80b993ca98baa37f4df5df2d23ef42f63e
Ana Marr <acarolinasim@gmail.com>
2020-04-04 13:02:18 +1030

    recipe styling

--- scriptRecipes.js
+++ scriptRecipes.js
@@ -15,10 +15,55 @@ function pullRecipes () {
     $.ajax(settings).done(function (response) {
         console.log(JSON.parse(response).results[0]);
         for (var i = 0; i < JSON.parse(response).results.length; i++) {//loops through the results and creates a card for each recipe found
-            $("#recipeContainer").append($('<li><div class="mb-3 border-solid border-l-8 border-yellow-500 rounded-t-lg rounded-b-lg"><div class="px-6 "><div id="title" class="font-bold text-xl ">'+JSON.parse(response).results[i].title+'</div><img src="'+JSON.parse(response).results[i].thumbnail+'" alt="'+JSON.parse(response).results[i].title+'"><p id="description" class="text-gray-700 text-base">Ingredients: '+JSON.parse(response).results[i].ingredients+'</p></div><div class="px-6 py-4"><span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"><a target="blank" class="linkstyle" href="'+JSON.parse(response).results[i].href+'">Cook Me</a></span></div></div></li>'));
+            $("#recipeContainer").append(
+                
+                
+            $('<li>' +
+            
+            '<div class="max-w-sm w-full lg:max-w-full lg:flex">' +
+            
+            '<div class=" flex items-center lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">' +
+            '<img class="w-40 h-50 rounded mr-4" src="'+JSON.parse(response).results[i].thumbnail+'" alt="'+JSON.parse(response).results[i].title+'"></img>' + 
+            '</div>'  +
+
+            '<div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">' +
+            
+            
+            '<div class="text-gray-900 font-bold text-xl mb-2">'+JSON.parse(response).results[i].title+'</div>' +
+            '<p class="text-gray-700 text-base">Ingredients: '+JSON.parse(response).results[i].ingredients+'</p>' +
+
+            '<div class="px-6 py-4"><span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">'+
+            '<a target="blank" class="linkstyle" href="'+JSON.parse(response).results[i].href+'">Cook Me</a></span>'+
+            
+           '</div>' +
+
+           '</div>' +
+            
+        
+            
+           '</li>'));
+                
+            
+            
         }
+
     });
 
 }
 
-// border-solid border-l-8 border-yellow-500 rounded-t-lg rounded-b-lg
+
+/* $('<li>' +
+            
+            '<div class="mb-3 border-solid border-l-8 border-yellow-500 rounded-t-lg rounded-b-lg">' +
+            
+            '<div class="px-6 ">' +
+                
+            '<div id="title" class="font-bold text-xl ">'+JSON.parse(response).results[i].title+'</div>' +
+            '<img src="'+JSON.parse(response).results[i].thumbnail+'" alt="'+JSON.parse(response).results[i].title+'">' + 
+            '<p id="description" class="text-gray-700 text-base">Ingredients: '+JSON.parse(response).results[i].ingredients+'</p></div>' + 
+            
+          '<div class="px-6 py-4"><span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"><a target="blank" class="linkstyle" href="'+JSON.parse(response).results[i].href+'">Cook Me</a></span></div>' + 
+            
+           '</div>' + 
+            
+           '</li>'));*/
